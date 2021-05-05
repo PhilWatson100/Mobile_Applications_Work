5eb612a7a3260adbd07ea6316700608fe3b00b65
PhilWatson100 <Phil100Watson100@gmail.com>
2021-05-05 02:22:51 +0100

    adding more stuff
    no discription

---
+++ 2nd lab/duck.ts
@@ -0,0 +1,7 @@
+function addition(value: string) {
+    console.log("value is : " + value);
+}
+    let firstVal: number = 42;
+    let secondVal: number = 1;
+    let sumOfVals: string = (firstVal + secondVal).toLocaleString();
+    addition(sumOfVals);
---
+++ 2nd lab/types.js
@@ -0,0 +1,16 @@
+var num1 = 5;
+console.log(num1);
+var flag = false;
+console.log(flag);
+var colour = "Red";
+console.log(colour);
+var list = ["hi", "hello", "hey"];
+for (var i = 0; i < list.length; i++) {
+    console.log("string " + list[i] + " . ");
+}
+var notSure = 5;
+console.log(notSure);
+var listany = [2, true, "test"];
+for (var i = 0; i < listany.length; i++) {
+    console.log("Array is " + listany[i] + " . ");
+}
